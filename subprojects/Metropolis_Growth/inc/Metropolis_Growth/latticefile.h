8f790d86c55aa4f041ba58b319cdcfec29dff383
John <john@ubuntu.(none)>
2012-08-02 13:17:12 -0700

    Included, to the git commit, the files necessary for the previous update to work

---
+++ subprojects/Metropolis_Growth/inc/Metropolis_Growth/latticefile.h
@@ -0,0 +1,53 @@
+//latticefile.h
+
+#ifndef LATTICEFILE_H
+#define LATTICEFILE_H
+
+#include <vector>
+#include "filehandler.h"
+
+class Lattice;
+
+class LatticeFile : public FileHandler{
+ public:
+  typedef std::vector<double> RGBVect;
+  enum Color { kColorRed, kColorBlue, kColorGreen, kColorOrange, kColorGrey, kColorBlack };
+  //Friendship because of the private ctor.
+  friend class Lattice;
+
+ private:
+  Lattice& m_lattice;
+
+ public:
+  void PrepareLatticeFile();
+
+ private:
+  LatticeFile(Lattice& lattice_to_save);
+
+ public:
+  //MakeSimpleFile() saves only the rot and occ data
+  void MakeSimpleFile(){};
+
+  //MakeBondFile() saves the rot and occ data, as well
+  // as the number of active bonds at each site
+  void MakeBondNumberFile(Interaction& lattice_interaction){};
+
+  //MakeColorImage() uses the interactions N1 and N2 to create a gnuplot file
+  void MakeSquareLatticeColorImage(Interaction& lattice_interaction)
+  {std::cout << "Dummy function for FileHandler::MakeSquareLatticeColorImage.";}
+
+ private:
+  /*
+  //ColorLookup uses two different return types: return by value (RGBVect) or return by pointer (optional).
+  RGBVect ColorLookup(Color c, double* r = 0, double* g = 0, double* b = 0);
+  //Hard-coded assumptions about which micro-phases should be output as which colors
+  Color SelectSiteColor(int n1_bonds, int n2_bonds);
+  //Draws a single site at the coordinate specified.
+  // Note: Pixel_size works better as an odd number.
+  void DrawSite(pngwriter& png, int pixel_size, vector<int> coord, int rot, int R, int n1_bonds, int n2_bonds);
+  */
+  void InsertHeader(){}
+
+};
+
+#endif // LATTICEFILE_H
---
+++ subprojects/Metropolis_Growth/inc/Metropolis_Growth/orderparamfile.h
@@ -0,0 +1,56 @@
+//orderparamfile.h
+#ifndef __ORDERPARAMFILE_H_INCLUDED__
+#define __ORDERPARAMFILE_H_INCLUDED__
+
+#include <vector>
+#include "filehandler.h"
+#include "logfile.h"
+
+class MonteCarlo;
+
+//TODO: (jhaberstroh@lbl.gov) Include a Memento pattern for saving m_montecarlo to file.
+//Note: This class has a private constructor and should only be created by
+//      a MonteCarlo object!
+//Furthermore, this file has no default constructor. Because it must be created
+// by a MonteCarlo object, that MonteCarlo object must pass itself by reference.
+//Moreover, a OrderParamFile should never be passed from one MonteCarlo object
+// to another. A MonteCarlo object must create its own OrderParamFile.
+class OrderParamFile : public FileHandler{
+ public:
+  //Friendship because of the private ctor.
+  friend class MonteCarlo;
+  /*--------------------------------------------------
+    Member Variables
+    --------------------------------------------------*/
+ private:
+  MonteCarlo& m_montecarlo;
+  //m_log_file is owned by the MonteCarlo object which is controlling the OrderParamFile
+  //As of 08/01/21: m_log_file is logging the names of the current image files
+  LogFile* m_log_file;
+  /*--------------------------------------------------
+    Constructor
+    --------------------------------------------------*/
+ private:
+  OrderParamFile(MonteCarlo& mc_save, LogFile* log_file = 0);
+  void PrepareFile(const std::vector<FNameOpt>& fname_include = std::vector<FNameOpt>(),
+		     std::ios::openmode write_openmode = std::ios::app);
+  /*--------------------------------------------------
+    Member Functions
+    --------------------------------------------------*/
+ protected:
+  std::string MakeFileName(const std::vector<FNameOpt>& fname_include);
+
+  //Requires the m_write_file to be currently open.
+  void InsertHeader();
+
+
+ public:
+  void Track();
+
+  //Outsources to gnuplot
+  int MakeImage(FColumn y_axis = kN1OP);
+
+  void MakeLatticeImage();
+};
+
+#endif // __ORDERPARAMFILE_H_INCLUDED__
